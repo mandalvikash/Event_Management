d642dfda2335c14419b24cda1b2ac59becb1148d
mandalvikash <146217029+mandalvikash@users.noreply.github.com>
2025-02-09 10:23:29 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({https://event-management-xi-eight.vercel.app/}));
 app.use(express.json());
 
 console.log(process.env.MONGO_URL)
